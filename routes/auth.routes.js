15d618765ea8d45ced3319ae48c0cc755c25b876
Christian Kroer Sørensen <dkchkrso@lego.com>
2022-02-02 16:53:08 +0100

    Iteration 2 completed

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,19 +1,13 @@
-// routes/auth.routes.js
-
 const { Router } = require("express");
 const router = new Router();
-
 const bcryptjs = require("bcryptjs");
 const saltRounds = 10;
-
 const User = require("../models/User.model");
 
-// GET route ==> to display the signup form to users
+// S I G N U P
 router.get("/signup", (req, res) => res.render("auth/signup"));
 
-// POST route ==> to process form data
 router.post("/signup", (req, res, next) => {
-
   const { username, password } = req.body;
 
   bcryptjs
@@ -22,17 +16,61 @@ router.post("/signup", (req, res, next) => {
     .then((hashedPassword) => {
       return User.create({
         username,
-        password: hashedPassword
+        password: hashedPassword,
       });
     })
     .then((userFromDB) => {
-      console.log("Newly created user is: ", userFromDB)
+      console.log("Newly created user is: ", userFromDB);
       res.redirect("/userProfile");
     })
-    
+
     .catch((error) => next(error));
 });
 
-router.get("/userProfile", (req, res) => res.render("users/user-profile"));
+router.get("/userProfile", (req, res) => {
+  res.render("users/user-profile", { userInSession: req.session.currentUser });
+});
+
+// L O G I N
+
+router.get("/login", (req, res) => res.render("auth/login"));
+
+router.post("/login", (req, res, next) => {
+  //console.log("SESSION =====> ", req.session);
+  const { username, password } = req.body;
+
+  if (username === "" || password === "") {
+    res.render("auth/login", {
+      errorMessage: "Please enter both, username and password to login.",
+    });
+    return;
+  }
+
+  User.findOne({ username })
+    .then((user) => {
+      //console.log(user)
+      if (!user) {
+        res.render("auth/login", {
+          errorMessage: "Username is not registered.",
+        });
+        return;
+      } else if (bcryptjs.compareSync(password, user.password)) {
+        req.session.currentUser = user;
+        res.redirect("/userProfile");
+      } else {
+        res.render("auth/login", { errorMessage: "Incorrect password." });
+      }
+    })
+    .catch((error) => next(error));
+});
+
+// L O G O U T
+
+router.post("/logout", (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) next(err);
+    res.redirect("/");
+  });
+});
 
 module.exports = router;
